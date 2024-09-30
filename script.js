830b2647c59404624bd534a97a4c4303f63b947f
Micaías Viola <micaiasviola@users.noreply.github.com>
2024-09-30 09:50:00 -0300

    v3

--- script.js
+++ script.js
@@ -9,3 +9,22 @@ function redirectToWebsite() {
     const websiteUrl = "https://www.yourcompany.com";  // Replace with the correct URL
     window.open(websiteUrl, '_blank');
 }
+
+const images = [
+    'url("./imgs/imagem1.jpg")',
+    'url("./imgs/imagem2.jpg")', // Adicione o caminho correto
+    'url("./imgs/imagem3.jpg")'  // Adicione o caminho correto
+];
+
+let currentIndex = 0;
+
+function changeBackground() {
+    document.body.style.backgroundImage = images[currentIndex];
+    currentIndex = (currentIndex + 1) % images.length;
+}
+
+// Troca de imagem a cada 5 segundos
+setInterval(changeBackground, 5000);
+
+// Inicializa com a primeira imagem
+changeBackground();
